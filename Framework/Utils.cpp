9d3e83d8edf3286bc25abba4a33c3f4ea4b95338
realstone513 <ting6247@nate.com>
2022-10-25 17:51:25 +0900

    fix random

--- Framework/Utils.cpp
+++ Framework/Utils.cpp
@@ -44,7 +44,7 @@ float Utils::Clamp(float v, float min, float max)
 
 int Utils::RandomRange(int min, int maxInclude)
 {
-	return (gen() % (maxInclude - min)) + min;
+	return (gen() % (maxInclude - min)) + min + 1;
 }
 
 float Utils::RandomRange(float min, float maxInclude)
